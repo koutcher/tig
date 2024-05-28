koutcher/tig

commit 935fdbaa0ec6a182ab18016e0f3d0aa29bdf544b

Open the blob corresponding to the diff line
Fixes #1334

src/diff.c
@@ -633,7 +633,7 @@ diff_get_lineno(struct view *view, struct line *line, bool old)
 }
 
 static enum request
-diff_trace_origin(struct view *view, struct line *line)
+diff_trace_origin(struct view *view, enum request request, struct line *line)
 {
 	struct line *commit_line = find_prev_line_by_type(view, line, LINE_COMMIT);
 	char id[SIZEOF_REV];
@@ -704,10 +704,10 @@ diff_trace_origin(struct view *view, struct line *line)
 	}
 
 	string_ncopy(view->env->file, commit.filename, strlen(commit.filename));
-	string_copy(view->env->ref, header.id);
+	string_copy(request == REQ_VIEW_BLAME ? view->env->ref : view->env->commit, header.id);
 	view->env->goto_lineno = header.orig_lineno - 1;
 
-	return REQ_VIEW_BLAME;
+	return request;
 }
 
 const char *
@@ -782,7 +782,8 @@ diff_request(struct view *view, enum request request, struct line *line)
 {
 	switch (request) {
 	case REQ_VIEW_BLAME:
-		return diff_trace_origin(view, line);
+	case REQ_VIEW_BLOB:
+		return diff_trace_origin(view, request, line);
 
 	case REQ_EDIT:
 		return diff_common_edit(view, request, line);

commit 94cd7e1a87b969916fc6657d2dbc294765f91fdf

Fix opening the blob view for new files
Regression introduced in 935fdbaa0ec6a182ab18016e0f3d0aa29bdf544b.

src/diff.c
@@ -652,16 +652,9 @@ diff_trace_origin(struct view *view, enum request request, struct line *line)
 		return REQ_NONE;
 	}
 
-	for (; diff < line && !file; diff++) {
-		const char *data = box_text(diff);
+	file = diff_get_pathname(view, line, line->type == LINE_DIFF_DEL);
 
-		if (!prefixcmp(data, "--- a/")) {
-			file = data + STRING_SIZE("--- a/");
-			break;
-		}
-	}
-
-	if (diff == line || !file) {
+	if (!file) {
 		report("Failed to read the file name");
 		return REQ_NONE;
 	}