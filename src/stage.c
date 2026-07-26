c0d85bcf8a10f191a10407d038520fc3e2d3c713
Thomas Koutcher <thomas.koutcher@online.fr>
2026-07-26 20:20:55 +0200

    Fix compilation warning in macOS CI

--- src/stage.c
+++ src/stage.c
@@ -347,7 +347,7 @@ stage_insert_chunk(struct view *view, struct chunk_header *header,
 		const char *text = box_text(line);
 
 		if (!add_line_text_at(view, after_lineno++, text, line->type, 1))
-			return false;
+			return NULL;
 	}
 
 	return view->line + after_lineno;
